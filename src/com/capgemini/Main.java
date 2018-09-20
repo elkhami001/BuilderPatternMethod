64d2da89ed61811ac83097d46273b81f572675f7
elkhami001 <wzkvdnw1>
2018-09-20 02:16:21 +0200

    Refactor Main

--- src/com/capgemini/Main.java
+++ src/com/capgemini/Main.java
@@ -4,22 +4,27 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //Car myCar = new Car();
+        CarBuilder raceStyleCar = new RaceCarBuilder();
 
-
-	    CarBuilder raceStyleCar = new RaceCarBuilder();
-
-        // The Director can be seen as a car engineer who oversees the construction of the car
+        // The Director can be seen as
+        // a car engineer who oversees
+        // the construction of the car
 	    CarDirector carDirector = new CarDirector(raceStyleCar);
 
 	    carDirector.makeCar();
 
 	    Car firstCar = carDirector.getCar();
 
-        System.out.println("Car built");
-        System.out.println("Car body type: " + firstCar.getCarBody());
-        System.out.println("Car amount of doors: " + firstCar.getCarDoors());
-        System.out.println("Car engine type: " + firstCar.getCarEngine());
-        System.out.println("Car color: " + firstCar.getCarColor());
+        System.out.println(
+                "Car built"
+                        +"Car body type: "
+                        + firstCar.getCarBody()
+                        +"Car amount of doors: "
+                        + firstCar.getCarDoors()
+                        +"Car engine type: "
+                        + firstCar.getCarEngine()
+                        +"Car color: "
+                        + firstCar.getCarColor()
+        );
     }
 }
